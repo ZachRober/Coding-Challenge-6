ddc1eff16c793f5f10b76d8f0631dfb7925366c3
ZachRober <nolethedgehogdie@gmail.com>
2024-09-17 18:13:45 -0400

    Initialize employees with shift schedules

---
+++ employee_shift_management.js
@@ -0,0 +1,4 @@
+const employees = [{name: "Amy",shifts:[{Day:"Monday",Hours:6},{Day:"Thursday",Hours:4},{Day:"Saturday",Hours:8}]},
+    {name: "Bruce",shifts:[{Day:"Friday",Hours:8},{Day:"Saturday",Hours:8},{Day:"Sunday",Hours:6}]},
+    {name: "Charlie",shifts:[{Day:"Tuesday",Hours:6},{Day:"Thursday",Hours:4},{Day:"Saturday",Hours:8}]},
+    {name: "Daniel",shifts:[{Day:"Wednesday",Hours:6},{Day:"Thursday",Hours:4},{Day:"Sunday",Hours:6}]}]//creation of giant data set
