df7454213e9fc2b1f1aa4f605271de9b593830dc
ZachRober <nolethedgehogdie@gmail.com>
2024-09-17 22:26:15 -0400

    Create displayemployeeshifts function

--- employee_shift_management.js
+++ employee_shift_management.js
@@ -2,3 +2,13 @@ const employees = [{name: "Amy",shifts:[{Day:"Monday",Hours:6},{Day:"Thursday",H
     {name: "Bruce",shifts:[{Day:"Friday",Hours:8},{Day:"Saturday",Hours:8},{Day:"Sunday",Hours:6}]},
     {name: "Charlie",shifts:[{Day:"Tuesday",Hours:6},{Day:"Thursday",Hours:4},{Day:"Saturday",Hours:8}]},
     {name: "Daniel",shifts:[{Day:"Wednesday",Hours:6},{Day:"Thursday",Hours:4},{Day:"Sunday",Hours:6}]}]//creation of giant data set
+
+function displayEmployeeShifts(x){
+    let employeeName = x.name;
+    let values = Object.values(x.shifts);
+    console.log(`${employeeName} works`);
+    values.forEach(shift=>{console.log(`${shift.Day} for ${shift.Hours} hours`)
+    });
+    return;
+}
+displayEmployeeShifts(employees[0]);
