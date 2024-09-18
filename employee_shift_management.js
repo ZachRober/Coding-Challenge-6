3913d67ceffe1fd0b0176cdb02e4dbb034bfbed0
ZachRober <nolethedgehogdie@gmail.com>
2024-09-18 13:27:51 -0400

    create assignedshift function

--- employee_shift_management.js
+++ employee_shift_management.js
@@ -5,10 +5,27 @@ const employees = [{name: "Amy",shifts:[{Day:"Monday",Hours:6},{Day:"Thursday",H
 
 function displayEmployeeShifts(x){
     let employeeName = x.name;
-    let values = Object.values(x.shifts);
+    let values = x.shifts;
     console.log(`${employeeName} works`);
-    values.forEach(shift=>{console.log(`${shift.Day} for ${shift.Hours} hours`)
+    values.forEach(shift=>{console.log(`${shift.Day} for ${shift.Hours} hours`)//iterates over each shift and displays the time and day
     });
     return;
 }
 displayEmployeeShifts(employees[0]);
+
+function assignedShift(x,y,z){
+    let returnedName = employees.find(employee=>employee.name===x);
+    let index = employees.findIndex(employee=>employee.name===x);//returns index of the employee
+    let neededShifts = returnedName.shifts; //returns array of just the shifts for the given employee
+    if (neededShifts.some(shift=>shift.Day===y)){
+        console.log("Employee already works that day");//checks for shifts that day
+    }
+    else{
+        let newShift = {Day:y,Hours:z};
+        return employees[index].shifts.push(newShift);//adds the newshift to the shifts
+    }
+    return;
+}
+assignedShift("Charlie","Thursday",6);
+
+
